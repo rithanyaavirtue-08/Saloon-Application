f4854a1cc90b735be8b7753ad496cbbadaeb551b
Rithanyaa <rithanyaa.virtue@gmail.com>
2026-06-07 16:08:16 +0530

    POST ,GET,PULL,DELETE

--- src/main/java/zen/co/controller/UserController.java
+++ src/main/java/zen/co/controller/UserController.java
@@ -1,14 +1,12 @@
 package zen.co.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zen.co.model.User;
 import zen.co.repository.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -27,6 +25,27 @@ public class UserController {
     public List<User> getUsers(){
         return userRepository.findAll();
     }
-    public
+    @GetMapping("/api/users/{userId}")
+    public User getUserById(@PathVariable("userId")  long id) throws Exception {
+        Optional<User> otp=userRepository.findById(id);
+        if(otp.isPresent()){
+            return otp.get();
+        }
+        throw new Exception("User not found with id: "+id);
+    }
+
+     @PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user,@PathVariable long id) throws Exception {
+        Optional <User> otp=userRepository.findById(id);
+        if(otp.isEmpty()){
+            throw new Exception("User not found with id:"+id);
+        }
+        User existingUser=otp.get();
+        existingUser.setFullName(user.getFullName() );
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+
+        return userRepository.save(existingUser);//update by id ,id is in existing user
+     }
 
 }
--- src/main/java/zen/co/model/User.java
+++ src/main/java/zen/co/model/User.java
@@ -11,7 +11,6 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     private String fullName;
     private String email;
     private String phone;
