d6c8d970096391f66e78cbe47f62901a63c7120d
Rithanyaa <rithanyaa.virtue@gmail.com>
2026-06-09 23:49:00 +0530

    Dev

--- src/main/java/zen/co/controller/HomeController.java
+++ src/main/java/zen/co/controller/HomeController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-
     @GetMapping("/home")
     public String home(){
         return "working";
